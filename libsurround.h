706ab5c4a59210aac62eee4a80b96cbd6d9762eb
pierce <pierceoburger@icloud.com>
2023-07-12 02:59:26 -0400

    added sur_image_init_from_file

--- libsurround.h
+++ libsurround.h
@@ -74,7 +74,7 @@ int unmap_window(/*params*/);
 */
 typedef enum{false,true} bool;
 
-#define curr(fd) lseek(fd,0,SEEK_CUR);
+#define curr(fd) (int)lseek(fd,0,SEEK_CUR)
 
 /**
  * @brief prints an error message and exits with EXIT_FAILURE
@@ -236,6 +236,12 @@ int set_pixel(uint32_t x, uint32_t y, rgb_color_t color, bmp_image_t image);
 bmp_image_t create_bmp(const char* filename, uint32_t width, uint32_t height, uint32_t xres, uint32_t yres);
 
 
+static int sur_image_init_bmp_from_file(sur_image_t* image);
+	//nested function in sur_image_init_from_file
+
+
+
+
 #include "ename.c.inc"
 void err_exit(const char* format,...){
 	va_list args;
@@ -325,9 +331,9 @@ int set_pixel(uint32_t x, uint32_t y, rgb_color_t color, bmp_image_t image){
 }
 
 bmp_image_t create_bmp(const char* filename,unsigned int width, unsigned int height,unsigned int xres, unsigned int yres){
-	FILE* fp=fopen("image.bmp","w+");
+	FILE* fp=fopen(filename,"w+");
 	fpos_t fpos;
-	unsigned int image_size=width*height*4;
+	unsigned int image_size = height*(width*3 + 4 - (3 * width) % 4);
 	unsigned int var=0x36+image_size;
 	fputc('B',fp);
 	fputc('M',fp);
@@ -386,37 +392,154 @@ sur_image_t sur_image_init(uint32_t width,uint32_t height){
 
 sur_image_t sur_image_init_from_file(const char* pathname){
 	sur_image_t image;
-	image.pathname = malloc(strlen(pathname)*sizeof(char));
-	if(!image.pathname)
-		err_exit("malloc");
-	strcpy(image.pathname,pathname);
-
-	int fd = open(pathname,O_RDONLY);
+	int fd = open(pathname,O_RDONLY);// open file for read only
 	if(fd == -1)
 		err_exit("open");
+	image.pathname = malloc(strlen(pathname)*sizeof(char));
+	// TODO: in the free function make sure to free this as well
+
+	if(!image.pathname)
+		err_exit("malloc");
+
+	image.fd = (sur_file_t)fd;
+	strcpy(image.pathname,pathname);
+
+
 
 	// typically the first two bytes of the file are used to specify file format
 	char buffer[3];
 	if(read(fd,buffer,2) == -1)
 		err_exit("read");
 	buffer[2]='\0';
-	if(buffer == "BM"){// is a .bmp file
+	if(!strcmp(buffer,"BM")){// is a .bmp file
 		printf("sur_image_init_from_file: %s is a .bmp file\n", pathname);
-					   // TODO: implement this
-					   // TODO: test this works
-
-			
+		sur_image_init_bmp_from_file(&image);//TODO: add a return checker
 	}
 	// TODO: implement handlers for more file formats
 	else{
 		printf("sur_image_init_from_file: unrecognized file format\n");
 		exit(EXIT_FAILURE);
 	}
-	if(close(fd))
-		err_exit("close");
 	return image;
 }
 
+
+static int sur_image_init_bmp_from_file(sur_image_t* image){
+	int fd = (int)image->fd;
+	int data_offset;
+	int image_size;
+	int filesize;
+	int calc_size;
+	int offset;
+	uint32_t temp;
+/*	uint32_t temp7;
+	uint32_t temp8;
+	uint32_t temp9;
+	*/
+					   // TODO: implement this
+					   // TODO: test this works
+		
+		if(read(fd,&filesize,4) == -1)// getting filesize
+			err_exit("read");
+		if(lseek(fd,4,SEEK_CUR) == -1)// unused area
+			err_exit("lseek");
+		if(read(fd,&data_offset,4) == -1)// offset of pixel data
+			err_exit("read");
+		if(read(fd,&temp,4) == -1)//size of infoheader, should be 40, throw error if not 40
+			err_exit("read");
+		if(temp != 40){
+			printf("error reading file on line %d\n",__LINE__);
+			exit(EXIT_FAILURE);
+		}
+		if(read(fd,&(image->width),4) == -1)// read width into image.width
+			err_exit("read");
+
+//		printf("width of image is %d pixels\n",image->width);
+
+		if(read(fd,&(image->height),4) == -1)// read height into image.height
+			err_exit("read");
+
+//		printf("height of image is %d pixels\n",image->height);
+
+		if(read(fd,&temp,2) == -1)//number of planes, should be 1, throw error if not 1
+			err_exit("read");
+		if(temp != 1){
+			printf("error reading file on line %d\n",__LINE__);
+			exit(EXIT_FAILURE);
+		}
+		if(read(fd,&temp,2) == -1)//bits per pixel, should be 24, throw error if not 24
+								  //TODO: add compatibility with other values {1, 4, 8, 16, 24}
+			err_exit("read");
+
+		//here temp is the bits per pixel
+
+		if(read(fd,&temp,4) == -1)
+			err_exit("read");
+			// compression algorithm, see bmp file format website for specifics
+			// currently unused, TODO: implement compression and decompression algorithms
+
+		//here temp corresponds to a compression algorithm
+		
+		if(read(fd,&image_size,4) == -1)// image size in bytes of the data part of the file
+			err_exit("read");
+		calc_size = image->height * (image->width*3 + 4 - (image->width * 3) % 4);
+		if(!(image_size == calc_size)){//error with image
+			printf("error reading file on line %d\n",__LINE__);
+			exit(EXIT_FAILURE);
+		}
+
+		if(lseek(fd,8,SEEK_CUR) == -1)
+			//skipping over the horizontal and vertical resolution (not needed here)
+			err_exit("lseek");
+
+
+
+
+		if(read(fd,&temp,4) == -1)//number of colors in image, important for color table
+								  //TODO: currently not implementation for this
+			err_exit("read");
+
+		//here temp is the number of colors in the image
+		
+		if(lseek(fd,4,SEEK_CUR) == -1)//unimportant
+			err_exit("lseek");
+
+		//here there should be a colortable for certain values of 'bpp' 
+		// ( important for something yet to implement )
+		
+		//printf("data offset is %d\n",data_offset);
+		if(lseek(fd,data_offset,SEEK_SET) == -1)
+			err_exit("lseek");
+		
+		image->data = malloc(image->height * sizeof(uint32_t*));
+		if(image->data == NULL)
+			err_exit("malloc");
+		for(int i = 0;i < image->height;i++){
+			image->data[i] = malloc(image->width * sizeof(uint32_t));
+			if(image->data[i] == NULL)
+				err_exit("malloc");
+			for(int j = 0; j<image->width;j++){
+				offset = data_offset + 3*j + (image->height - i-1) * (3 * image->width + (4 - 3*image->width%4));
+				//offset of pixel neccessary
+				if(lseek(fd,offset,SEEK_SET) == -1)
+					err_exit("lseek");
+				if(read(fd,&image->data[i][j],3) == -1)
+					err_exit("lseek");
+				/*
+				temp7 = image->data[i][j] & 0xff;
+				temp8 = image->data[i][j] & 0xff00;
+				temp9 = image->data[i][j] & 0xff0000;
+				
+				image->data[i][j] = (temp7 << 16) | (temp8) | (temp9 >> 16);
+				*/
+			}
+
+		}
+
+
+
+	return 0;
+}
 int sur_image_save_to_file(sur_image_t image,uint32_t xres, uint32_t yres, int compression,
 		const char* filename, const char* filetype){
 	int fd = open(image.pathname, O_RDWR);
@@ -424,6 +547,6 @@ int sur_image_save_to_file(sur_image_t image,uint32_t xres, uint32_t yres, int c
 		err_exit("open");
 	// TODO: actually make this function do its intended purpose
 	if(close(fd))
-		err_exit("open");
+		err_exit("close");
 	return 0;
 }
--- test.c
+++ test.c
@@ -1,21 +1,29 @@
 #include "libsurround.h"
 
-int main(){
-	
+rgb_color_t red = {0, 0, 255};
+rgb_color_t blue = {255, 0, 0};
+
+void make_image(){//makes a 5x5 image with old api; all red image
 	char *filename="test.bmp";
-	rgb_color_t red = {0, 0, 255};
-	printf("red is %x\n",red);
+//	printf("red is %x\n",red);
 	bmp_image_t image=create_bmp(filename,5,5,5,5);
 	for(int i = 0; i < 5; i++){
 		for(int j = 0; j < 5; j++){
 			set_pixel(i,j,red,image);
 		}
 	}
+	set_pixel(1,1,blue,image);
+	fclose(image.fptr);//upside down, will make pixel blue at (3,1)
+}
 
-
-	fclose(image.fptr);
-	
-	
+int main(){
+	make_image();
+	sur_image_t image = sur_image_init_from_file("test.bmp");
+	for(int i=0;i<image.height;i++){
+		for(int j=0;j<image.width;j++){
+			printf("image pixel (%d,%d) is 0x%06x\n",i,j,image.data[i][j]);
+		}
+	}
 	printf("it compiled!!\n");
 	
 	return 0;
